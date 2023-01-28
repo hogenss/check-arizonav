47a900ea86da3d69e6922825aed426d2fe7d1732
HogenSS <81771450+hogenss@users.noreply.github.com>
2023-01-29 01:07:46 +0300

    Update check-arizonav.py
    Added code notes

--- check-arizonav.py
+++ check-arizonav.py
@@ -1,38 +1,55 @@
+# by HogenSS
+# vk.com/sergey_hogen
 import undetected_chromedriver.v2 as uc
 from selenium.webdriver.common.by import By
 import re
-options = uc.ChromeOptions()
-#options.headless=True
-#options.add_argument('--headless')
+
+#Установка настроек
+options = uc.ChromeOptions() 
+options.headless=False
+options.add_argument('--headless') # Скрытие браузера (имеются с этим проблемы)
+
+# Консоль
 print('Укажите никнейм')
 print('Важно: Указывайте никнейм такой же, какой он есть на форуме.')
-nickname = input()
-driver = uc.Chrome(options=options)
+nickname = input() # Ввод никнейма
+driver = uc.Chrome(options=options) #Вывод хрома
+print('Идёт подсчёт жалоб...')
+
+# Переменные
 page = 1
 val = 0
 
+# Бесконечный цикл
 while True:
     try:
+        # Открытие браузера + поиск объекта
         driver.get("https://forum.arizona-v.com/forums/129/page-"+ str(page) + "?last_days=7")
         element = driver.find_elements(By.CLASS_NAME, "structItem")
         sa = driver.find_elements(By.CLASS_NAME, "pageNav-jump--next")
+
+        # Цикл обработки информации
         for e in element:
-            res = re.findall(r'Закреплено', e.text)
-            resu = re.findall(r'Закрыта', e.text)
+            res = re.findall(r'Закреплено', e.text) # Проверка закреплена ли жалоба
+            resu = re.findall(r'Закрыта', e.text) # Проверка открыта ли жалоба
             if res == []:
                 if resu != []:
-                    s = re.finditer(r"Просмотры\s(.*)()(мин. назад|[0-9]{1,4})\s([A-z _]{1,30})", e.text, flags=re.S)
+                    s = re.finditer(r"Просмотры\s(.*)()(мин. назад|[0-9]{1,4})\s([A-z _]{1,30})", e.text, flags=re.S) # Поиск никнейма кто закрыл жалобу
                     for nick in s:
                         d = nick.group(4)
-                        if d == nickname:
-                            val += 1
-                    
-            #print(e.text)
-        if(sa != []):
+                        if d == nickname: # Если введённый никнейм совпадает с форумом
+                            val += 1 # Прибавляем к значению жалоб
+
+        # Автоматическое перелистывание страниц
+        if(sa != []): 
             page += 1
         else:
             break
+    # error
     except Exception as ex:
         print(ex)
+
+# Вывод после выполнения кода
 print('Количество жалоб - ' + str(val))
+print('Для закрытия приложения нажмите: Ctrl + C')
 input()
